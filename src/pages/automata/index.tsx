cd04ca832f32001fb3224e5f979b82d0c1530cdd
Thrashy190 <diego456.dlm77@gmail.com>
2023-03-23 12:00:37 -0600

    add canva

---
+++ src/pages/automata/index.tsx
@@ -0,0 +1,12 @@
+import Canvas from "@/components/canvas";
+
+export default function Automata() {
+  return (
+    <>
+      <div className="text-xl">Automatas</div>
+      <div>
+        <Canvas></Canvas>
+      </div>
+    </>
+  );
+}
